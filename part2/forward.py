d8096dbf21633c299dd1b367a68f382d3fa9aef3
Abhisek Jana <adeveloperdiary@gmail.com>
2019-02-16 19:15:52 -0500

    data

--- part2/forward.py
+++ part2/forward.py
@@ -5,22 +5,29 @@ data = pd.read_csv('data_python.csv')
 
 V = data['Visible'].values
 
+# Transition Probabilities
 a = np.array(((0.54, 0.46), (0.49, 0.51)))
+
+# Emission Probabilities
 b = np.array(((0.16, 0.26, 0.58), (0.25, 0.28, 0.47)))
 
+# Equal Probabilities for the initial distribution
 initial_distribution = np.array((0.5, 0.5))
 
 
-def forward(X, a, b, initial_distribution):
-    alpha = np.zeros((X.shape[0], a.shape[0]))
-    alpha[0, :] = initial_distribution * b[:, X[0] - 1]
+def forward(V, a, b, initial_distribution):
+    alpha = np.zeros((V.shape[0], a.shape[0]))
+    alpha[0, :] = initial_distribution * b[:, V[0]]
 
-    for t in range(1, X.shape[0]):
+    for t in range(1, V.shape[0]):
         for j in range(a.shape[0]):
-            alpha[t, j] = alpha[t - 1].T.dot(a[j, :]) * b[j, V[t] - 1]
+            # Matrix Computation Steps
+            #                  ((1x2) . (1x2)) * (1)
+            #                  (1) * (1)
+            alpha[t, j] = alpha[t - 1].dot(a[j, :]) * b[j, V[t]]
 
     return alpha
 
 
-alpha = forward(X, a, b, initial_distribution)
+alpha = forward(V, a, b, initial_distribution)
 print(alpha)
